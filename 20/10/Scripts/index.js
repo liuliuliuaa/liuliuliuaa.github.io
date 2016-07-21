13145268e2661a48a34e57abbfa89048febfaa72
liuliuliuaa <759051032@qq.com>
2016-07-21 21:15:29 +0800

    样式

--- 20/10/Scripts/index.js
+++ 20/10/Scripts/index.js
@@ -5,13 +5,31 @@ $(function(){
      direction:"left",
      navId:"#nav",
      
-
-
-
    })
-
-
 })
+
+
+$(function(){
+	$("#partner footer .icon .red").hover(function() {
+		$(this).stop().fadeTo(500,0)
+		
+	}, function() {
+		$(this).stop().fadeTo(500,1)
+	});
+})
+
+
+
+
+
+	$(window).scroll(function(){
+			var top=$(this).scrollTop();
+				$("#header .navbox").css("top",top);						
+		});
+
+
+
+
 
 
 
