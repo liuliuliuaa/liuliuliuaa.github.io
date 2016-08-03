9c60e27a86c0018bb811d373107ad56b6673a7fa
liuliuliuaa <759051032@qq.com>
2016-08-03 16:49:20 +0800

    电子商务  首页

--- 20/18/js/sy.js
+++ 20/18/js/sy.js
@@ -9,4 +9,42 @@ $(function(){
 	})
 
 
+
+	// 回到顶部
+	$("#top").click(function(){
+		$("html,body").animate({"scrollTop":"0px"})
+	});
+
+
+
+	// 底部按钮变换
+	
+	$(".nainiu li").hover(function(){
+		var a=$(this).attr("value");
+		$(this).children("img").attr("src","images/f"+a+a+".png")
+	},
+
+	function(){
+		var a=$(this).attr("value");
+		$(this).children("img").attr("src","images/f"+a+".png")
+
+	}
+
+
+
+	);
+
+
+	// 图片hover
+	
+$(".women_pic1 li,.women_pic2 li,.men_pic1 li,.men_pic2 li,.children_pic1 li,.children_pic2 li").hover(function() {
+	$(this).children("span").fadeIn(500);
+
+
+
+}, function() {
+	$(this).children("span").fadeOut(500);
+	
+});
+
 })
