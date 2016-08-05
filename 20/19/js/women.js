d0026ee00a5690d1e518efde10b3bc0ab4c3a39c
liuliuliuaa <759051032@qq.com>
2016-08-05 13:16:32 +0800

    women2

--- 20/19/js/women.js
+++ 20/19/js/women.js
@@ -1,8 +1,8 @@
 $(function(){
+	// 产品展示图片
 	var b;
   $("#main ul li img").hover(function() {
     b=$(this).attr("src");
-  	console.log(b);
      var a=$(this).attr("attr");
      $(this).attr("src","images/"+a+".JPG");
       
@@ -10,4 +10,42 @@ $(function(){
      $(this).attr("src",b);
   
   });
+
+
+	// 回到顶部
+	$("#top").click(function(){
+		$("html,body").animate({"scrollTop":"0px"})
+	});
+
+
+
+	// 底部按钮变换
+	
+	$(".nainiu li").hover(function(){
+		var a=$(this).attr("value");
+		$(this).children("img").attr("src","images/f"+a+a+".png")
+	},
+
+	function(){
+		var a=$(this).attr("value");
+		$(this).children("img").attr("src","images/f"+a+".png")
+
+	});
+
+
+	// 搜索框点击改变宽度
+	// 获得焦点
+     $(".put1").focus(function(){
+         $(this).animate({"width":"150px"})
+
+     });
+   // 失去焦点
+    $(".put1").blur(function(){
+         $(this).animate({"width":"120px"})
+
+     });
+
+	
+
+
 })
